1dad5b1be92580e474696a34fd7c0bd99c7cbf44
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-07 10:33:33 +0200

    4 files modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 function Header() {
+  const matchProfile = useMatch('/profile');
+  const matchHomepage = useMatch('/');
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <div className="flex-fill">
         <strong> React-router </strong>
       </div>
       <ul className={styles.headerList}>
-        <Link to="/">Homepage</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/efzef">???</Link>
+        <NavLink end to="/">
+          Homepage
+        </NavLink>
+        <NavLink to="/profile">Profile</NavLink>
       </ul>
     </header>
   );
